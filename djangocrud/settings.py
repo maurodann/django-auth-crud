a4ee90a5a8b334938b43d09bf9ade715830d7e12
maurodann <maurodnaccarato@gmail.com>
2023-03-02 19:40:36 -0300

    database arreglado

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }
