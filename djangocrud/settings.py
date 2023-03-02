7f9a8fb0954b31c8b6252ea68cef99c603d9fd3c
maurodann <maurodnaccarato@gmail.com>
2023-03-02 19:12:38 -0300

    database problem

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/mysite',        
-            conn_max_age=600)    
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
